87569ef6cfc8416ebaaad46a4134e6c0f9534297
orgesified <gesishabani1@gmail.com>
2020-06-20 07:49:52 +0000

    Revert "f2fs: use wrapped IS_SWAPFILE()"
    This reverts commit 97aff98091454a1d9e31459baf875fe5b6c42c28.

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -3776,7 +3776,7 @@ static inline bool f2fs_force_buffered_io(struct inode *inode,
 				block_unaligned_IO(inode, iocb, iter))
 		return true;
 	if (is_sbi_flag_set(F2FS_I_SB(inode), SBI_CP_DISABLED) &&
-					!IS_SWAPFILE(inode))
+					!(inode->i_flags & S_SWAPFILE))
 		return true;
 
 	return false;
